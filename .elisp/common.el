7ec28a25df1ad52a145d9d6d207869d795cfb12a
İsmail Dönmez <ismail.donmez@vestel.com.tr>
2008-05-26 21:38:25 +0300

    Use Safari on MacOS

--- .elisp/common.el
+++ .elisp/common.el
@@ -51,9 +51,23 @@
 (ido-mode t)
 (setq ido-enable-flex-matching t)
 
-;; Use Firefox
-(setq browse-url-generic-program (executable-find "firefox")
-    browse-url-browser-function 'browse-url-generic)
+;; Use Firefox on X and Safari on MacOS
+(if (string= "x" window-system)
+    (progn
+      (setq browse-url-generic-program (executable-find "firefox")
+            browse-url-browser-function 'browse-url-generic))
+  (progn
+    (setq browse-url-browser-function 'browse-url-safari)
+    (defun browse-url-safari (url &optional new-window)
+      "Open URL in a new Safari window."
+      (interactive (browse-url-interactive-arg "URL: "))
+      (unless
+          (string= ""
+                   (shell-command-to-string
+                    (concat "open -a WebKit " url)))
+        (message "Starting Safari...")
+        (start-process (concat "open -a WebKit " url) nil "open -a WebKit " url)
+        (message "Starting Safari... done")))))
 
 ;; Server Name
 (setq server-name "ninbuntu")
