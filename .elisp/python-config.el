22378d00669355a668748fe1d4c308ae49b262f0
İsmail Dönmez <ismail.donmez@vestel.com.tr>
2008-06-01 22:16:36 +0300

    Remove pymacs

--- .elisp/python-config.el
+++ .elisp/python-config.el
@@ -1,10 +1,3 @@
 ;; Enable Python mode
 (autoload 'python "python-mode" "Python editing mode." t)
 
-;; Load ropemacs
-(require 'pymacs)
-(pymacs-load "ropemacs" "rope-")
-(setq ropemacs-enable-autoimport 't)
-
-;; Use Ctrl-A for code completion
-(global-set-key "\C-a" 'rope-code-assist)
