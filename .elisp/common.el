f3c1e8bc60c9c7e7ad881599431c6d507ad73584
İsmail Dönmez <ismail.donmez@vestel.com.tr>
2008-06-05 22:34:01 +0300

    always hide menubar

--- .elisp/common.el
+++ .elisp/common.el
@@ -33,8 +33,7 @@
 ;; Lose the UI
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (string= "x" window-system)
-    (if (fboundp 'menu-bar-mode) (menu-bar-mode -1)))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
 ;; For multi-tty mode
 (add-hook 'after-make-frame-functions
