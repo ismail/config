371c46e1d0da6cc49ca35be9e58a345bf8f49b40
İsmail Dönmez <ismail.donmez@vestel.com.tr>
2008-06-08 15:10:56 +0300

    Don't hide menubar in mac

--- .elisp/common.el
+++ .elisp/common.el
@@ -31,7 +31,9 @@
 ;; Lose the UI
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+(if (not (string= "ns" window-system))
+    (if (fboundp 'menu-bar-mode) (menu-bar-mode -1)))
 
 ;; For multi-tty mode
 (add-hook 'after-make-frame-functions
