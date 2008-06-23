7b0021068fd77aa23b014a75adcbcce325da6b1a
İsmail Dönmez <ismail.donmez@vestel.com.tr>
2008-06-23 07:49:56 +0300

    delete trailing whitespace

--- .elisp/common.el
+++ .elisp/common.el
@@ -10,6 +10,9 @@
 ;; No startup message
 (setq inhibit-startup-message 1)
 
+;; Die trailing whitespace die
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
 ;; Alternative keys for execute comment
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
