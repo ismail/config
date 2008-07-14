2f7f8982d626d148886716db016e071d9bd9715f
İsmail Dönmez <ismail.donmez@vestel.com.tr>
2008-07-14 11:01:35 +0300

    IPython

--- .elisp/common.el
+++ .elisp/common.el
@@ -20,6 +20,10 @@
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 
+;; IPython
+(setq ipython-command "/usr/bin/ipython")
+(require 'ipython)
+
 ;; Shortcut for comment/uncomment
 (global-set-key "\C-u" 'comment-or-uncomment-region)
 
