facfdaf5601635d4c0442510a3ee589c2bc18450
İsmail Dönmez <ismail.donmez@vestel.com.tr>
2008-08-01 12:56:27 +0300

    g-client is very very buggy

--- .elisp/google.el
+++
@@ -1,3 +0,0 @@
-(load-library "g")
-(setq g-user-email "ismail@namtrac.org")
-(setq g-html-handler 'w3m-buffer)
--- .emacs
+++ .emacs
@@ -24,6 +24,3 @@
 ;; Org mode
 (load "orgmode.el")
 
-;; G-Client
-(load "google.el")
-
