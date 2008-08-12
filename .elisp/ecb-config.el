049117dbe5a88baa873810ba98c879c35735832e
İsmail Dönmez <ismail.donmez@vestel.com.tr>
2008-08-12 15:15:45 +0300

    ECB fixes

--- .elisp/ecb-config.el
+++ .elisp/ecb-config.el
@@ -2,6 +2,8 @@
 (require 'ecb)
 (setq ecb-tip-of-the-day nil)
 (setq ecb-vc-enable-support t)
+(setq ecb-layout-name "left9")
+(setq ecb-primary-secondary-mouse-buttons (quote mouse-1--C-mouse-1))
 (setq semantic-load-turn-useful-things-on t)
 (semantic-load-enable-excessive-code-helpers)
 
