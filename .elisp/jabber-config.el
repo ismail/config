14d2b87c33c0d89c2e8bdcdc423eb728cb6450f5
İsmail Dönmez <ismail.donmez@vestel.com.tr>
2008-08-13 22:50:43 +0300

    still no luck

--- .elisp/jabber-config.el
+++ .elisp/jabber-config.el
@@ -1,9 +1,11 @@
 (require 'jabber)
 (setq jabber-account-list
-      (quote (("ismail@namtrac.org"
+      (quote (("ismail@namtrac.org/emacs"
                (:network-server . "talk.google.com")
-               (:jabber-server . "namtrac.org")
+               (:password . nil)
                (:port . 5223)
                (:connection-type . ssl)))))
+(setq jabber-mode-line-mode t)
 (setq fsm-debug nil)
+
 
