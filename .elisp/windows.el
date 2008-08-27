3f52496aef346d447d8924d02a2041af26d1d451
İsmail Dönmez <ismail.donmez@vestel.com.tr>
2008-08-27 17:15:04 +0300

    Don't mess with HOME

--- .elisp/windows.el
+++ .elisp/windows.el
@@ -1,6 +1,5 @@
 (let* ((cygwin-root "c:/cygwin")
        (cygwin-bin (concat cygwin-root "/bin")))
-  (setenv "HOME" (concat cygwin-root "/home/cartman"))
   (setenv "PATH" (concat cygwin-bin ";" (getenv "PATH")))
   (setq exec-path (cons cygwin-bin exec-path)))
 
