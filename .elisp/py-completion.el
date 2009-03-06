4c53ade6ef47dab62da139189283dace8f371537
İsmail Dönmez <ismail.donmez@vestel.com.tr>
2009-03-06 13:03:24 +0200

    Revert "Disable tabbing for the moment"
    This reverts commit c4ec0012bee304e27c4f9f5531b25511f44297e9.

--- .elisp/py-completion.el
+++ .elisp/py-completion.el
@@ -83,3 +83,6 @@
   (set (make-local-variable 'ac-auto-start) t))
 (defadvice ac-cleanup (after advice-turn-off-auto-start activate)
   (set (make-local-variable 'ac-auto-start) nil))
+
+(add-hook 'python-mode-hook (lambda ()
+    (define-key py-mode-map "\t" 'python-tab)))
