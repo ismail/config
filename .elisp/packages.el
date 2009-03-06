63d58036cd41b0991e6f78e98b09de67424a5d45
İsmail Dönmez <ismail.donmez@vestel.com.tr>
2009-03-06 12:52:08 +0200

    Python code completion via rope & autocomplete.el

--- .elisp/packages.el
+++ .elisp/packages.el
@@ -1,17 +1,11 @@
-;; YA snippet
-(require 'yasnippet)
-(yas/initialize)
-(yas/load-directory "~/.elisp/snippets")
-
 ;; Default content
 (require 'defaultcontent)
 
 ;; Enable twitter integration
 (require 'twit)
 
-;; Pysmell
-(require 'pysmell)
-(add-hook 'python-mode-hook (lambda () (pysmell-mode 1)))
+;; Python completion
+(load "py-completion.el")
 
 ;; C-a/C-b for open line above/below respectively
 (load "open-line.el")
@@ -23,15 +17,11 @@
 (autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
 (setq auto-mode-alist (cons '("\\.cs$" . csharp-mode) auto-mode-alist))
 
-;; Python mode
-(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
-(setq interpreter-mode-alist (cons '("python" . python-mode)
-                                       interpreter-mode-alist))
-(autoload 'python-mode "python-mode" "Python editing mode." t)
+;; Python completion
+(load "py-completion.el")
 
 ;; GIT emacs
 (require 'git)
-
 
 ;; IPython
 (setq ipython-command "/usr/bin/ipython")
