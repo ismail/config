9995d51fd534559ceeb60e6bfe3c22db4616606b
İsmail Dönmez <ismail@donmez.ws>
2013-04-25 20:36:54 +0200

    Add servethis alias for Windows

--- setup_cmd.bat
+++ setup_cmd.bat
@@ -5,5 +5,6 @@ doskey ls=dir
 doskey rm=del
 doskey ..=cd ..\..
 doskey ...=cd ..\..\..
+doskey servethis=python -m SimpleHTTPServer
 
 "C:\Program Files (x86)\clink\0.3\clink" inject --profile "C:\Users\cartman\AppData\Local\clink"
