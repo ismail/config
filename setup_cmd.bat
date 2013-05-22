e75dc08387faf3c918a60f79818e867a031113db
İsmail Dönmez <ismail@donmez.ws>
2013-05-22 23:07:51 +0200

    Add enc/dec for win32

--- setup_cmd.bat
+++ setup_cmd.bat
@@ -6,5 +6,7 @@ doskey rm=del
 doskey ..=cd ..\..
 doskey ...=cd ..\..\..
 doskey servethis=python -m SimpleHTTPServer
+doskey enc=gpg -ea -r
+doskey dec=gpg -d
 
 "C:\Program Files (x86)\clink\0.3\clink" inject --profile "C:\Users\cartman\AppData\Local\clink"
