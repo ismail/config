4888a95c8e7c8107c4219d8f2218bdd98e9cc278
İsmail Dönmez <ismail@i10z.com>
2014-09-26 15:20:03 +0300

    Update

--- setup_cmd.bat
+++ setup_cmd.bat
@@ -5,8 +5,8 @@ doskey ls=dir
 doskey rm=del
 doskey ..=cd ..\..
 doskey ...=cd ..\..\..
-doskey servethis=python -m SimpleHTTPServer
+doskey servethis=python -m http.server
 doskey enc=gpg -ea -r
 doskey dec=gpg -d
 
-"C:\Program Files (x86)\clink\0.3\clink" inject --profile "C:\Users\cartman\AppData\Local\clink"
+"C:\Program Files (x86)\clink\0.4\clink" inject --profile "C:\Users\ismail\AppData\Local\clink"
