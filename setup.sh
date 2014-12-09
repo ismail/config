ab750f7d932d687a2fde90dd35001231f3c36c4b
İsmail Dönmez <ismail@i10z.com>
2014-12-09 14:44:15 +0200

    Next time how about testing the code?

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ cd ~
 
 files=".dir_colors .gdb .gdbinit .gitconfig .gitignore .hgrc .irbrc .screenrc .tmux.conf .vim .vimrc .zshfuncs .zshinit .zshrc .zshrc-common"
 
-for f in files; do
+for f in $files; do
     ln -s config/$f
 done
