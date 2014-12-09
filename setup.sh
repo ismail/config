283190b1b45d5d47ce3956dc9a7b090b5b8f7ce2
İsmail Dönmez <ismail@i10z.com>
2014-12-09 14:53:53 +0200

    Also symlink custom zshrc if exists

--- setup.sh
+++ setup.sh
@@ -5,5 +5,10 @@ cd ~
 files=(.dir_colors .gdb .gdbinit .gitconfig .gitignore .hgrc .irbrc .screenrc .tmux.conf .vim .vimrc .zshfuncs .zshinit .zshrc .zshrc-common)
 
 for f in $files; do
-    ln -s config/$f
+    ln -sf config/$f
 done
+
+hostname=`hostname -s`
+if [ -f config/.zshrc-$hostname ]; then
+    ln -sf config/.zshrc-$hostname
+fi
