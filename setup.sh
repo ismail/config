450cf61f0fc04a3f8ec9dfd91650b27bddb137e3
İsmail Dönmez <ismail@i10z.com>
2014-12-09 14:57:30 +0200

    More config

--- setup.sh
+++ setup.sh
@@ -2,11 +2,25 @@
 
 cd ~
 
-files=(.dir_colors .gdb .gdbinit .gitconfig .gitignore .hgrc .irbrc .screenrc .tmux.conf .vim .vimrc .zshfuncs .zshinit .zshrc .zshrc-common)
+mkdir -p .ssh
+mkdir -p .gnupg
+
+files=(
+    .dir_colors .gdb .gdbinit .gitconfig .gitignore .hgrc .irbrc .mailcap .mutt .muttrc
+    .screenrc .tmux.conf .urlview .vim .vimrc .Xdefaults .zshfuncs .zshinit .zshrc .zshrc-common
+)
 
 for f in $files; do
     ln -sf config/$f
 done
+
+pushd .ssh
+ln -s ../config/.ssh/config
+popd
+
+pushd .gnupg
+ln -s ../config/.gnupg/gpg{-agent}.conf
+popd
 
 hostname=`hostname -s`
 if [ -f config/.zshrc-$hostname ]; then
