6810754c2f307c2c545b7d56a8aec7d63e7f59ed
İsmail Dönmez <ismail@i10z.com>
2014-12-09 14:43:11 +0200

    Fix

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 cd ~
 
-files = ".dir_colors .gdb .gdbinit .gitconfig .gitignore .hgrc .irbrc .screenrc .tmux.conf .vim .vimrc .zshfuncs .zshinit .zshrc .zshrc-common"
+files=".dir_colors .gdb .gdbinit .gitconfig .gitignore .hgrc .irbrc .screenrc .tmux.conf .vim .vimrc .zshfuncs .zshinit .zshrc .zshrc-common"
 
 for f in files; do
     ln -s config/$f
