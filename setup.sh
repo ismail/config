d3adb0e08a8422ee45502fa064fb7a30b4aee93a
İsmail Dönmez <ismail@i10z.com>
2015-11-16 13:01:39 +0200

    Do not to hardcode paths

--- setup.sh
+++ setup.sh
@@ -1,56 +1,57 @@
 #!/usr/bin/env zsh
 set -euo pipefail
 
-cd ~
+curdir=$(pwd)
+cd $HOME
 
 mkdir -p .ssh
 mkdir -p .gnupg
 
 files=(
-    .clang_format .gdb .gdbinit .gitconfig .gitignore .hgrc
+    .clang-format .gdb .gdbinit .gitconfig .gitignore .hgrc
     .irbrc .lftprc .mailcap .mutt_certificates .muttrc .screenrc 
     .tmux.conf .urlview .vimrc .Xdefaults .zshenv
     .zsh_fpath .zshfuncs .zshrc .zshrc-common
 )
 
 # ls(1) colors
-dircolors config/.dir_colors > ~/.lscolors
+dircolors $curdir/.dir_colors > ~/.lscolors
 
 [ ! -d ~/.vim/bundle/Vundle.vim ] &&
     rm -f ~/.vim &&
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 for f in $files; do
-    ln -sf config/$f
+    ln -sf $curdir/$f
 done
 
 pushd .ssh
-ln -sf ../config/.ssh/config
-cp ../config/.ssh/known_hosts .
+ln -sf $curdir/.ssh/config
+cp $curdir/.ssh/known_hosts .
 popd
 
 pushd .gnupg
-ln -sf ../config/.gnupg/dirmngr.conf
-ln -sf ../config/.gnupg/gpg.conf
-ln -sf ../config/.gnupg/gpg-agent.conf
+ln -sf $curdir/.gnupg/dirmngr.conf
+ln -sf $curdir/.gnupg/gpg.conf
+ln -sf $curdir/.gnupg/gpg-agent.conf
 popd
 
 case $(uname -s) in
     Linux*)
-        ln -sf config/.iftoprc
-        ln -sf config/.mpv
-        ln -sf config/.toprc
-        ln -sf config/.zshrc-linux
-        ln -sf config/.zshfuncs-linux
+        ln -sf $curdir/.iftoprc
+        ln -sf $curdir/.mpv
+        ln -sf $curdir/.toprc
+        ln -sf $curdir/.zshrc-linux
+        ln -sf $curdir/.zshfuncs-linux
         ;;
     CYGWIN_NT*)
-        ln -sf config/.minttyrc
-        ln -sf config/.mutt-gpg.rc
-        ln -sf config/.zshrc-windows
+        ln -sf $curdir/.minttyrc
+        ln -sf $curdir/.mutt-gpg.rc
+        ln -sf $curdir/.zshrc-windows
         sed -i s,"ControlMaster auto","ControlMaster no", ~/.ssh/config
         ;;
     Darwin*)
-        ln -sf config/.zshrc-mac
-        ln -sf config/.hushlogin
+        ln -sf $curdir/.zshrc-mac
+        ln -sf $curdir/.hushlogin
         ;;
 esac
