975ab9fc814e82ee75799d20227aaf1caae95d1a
İsmail Dönmez <ismail@i10z.com>
2021-04-05 14:42:00 +0200

    Make remote magenta

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,7 +1,11 @@
 function fish_prompt
     if [ $status -ne 0 ]
-        echo -n (set_color cyan)"; "
+        echo -n (set_color brcyan)"; "
     else
-        echo -n "; "
+        if test -n "$SSH_TTY"
+            echo -n (set_color brmagenta)"; "
+        else
+            echo -n (set_color bryellow)"; "
+        end
     end
 end
